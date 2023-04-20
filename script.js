c7ec468e30684458a616bd43206724c7f2582951
Arpit <jaiswalarpit1414@gmail.com>
2023-04-20 14:01:26 +0530

    Hper_clone

---
+++ script.js
@@ -0,0 +1,33 @@
+// hamburger javascript
+const hamburger = document.querySelector(".hamburger");
+const ham_container = document.querySelector(".container_drp");
+let elem = document.querySelector(".header-box");
+let x = document.querySelector(".header-img-container")
+hamburger.addEventListener("click", () => {
+    ham_container.style.transition = "top 5s ease";
+    if (elem.style.marginTop === "145px") {
+        elem.style.marginTop = "0px"
+    }
+    else {
+        elem.style.marginTop = "145px"
+    }
+    hamburger.classList.toggle("active");
+    ham_container.classList.toggle("active");
+
+})
+
+// sliderconatins text 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
---
+++ script_2.js
@@ -0,0 +1,55 @@
+let captcha_box = document.querySelector("#captcha-box");
+let refresh = document.querySelector(".captcha-refresh");
+
+const alphabetArray = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+];
+function generate_captcha() {
+    for (let i = 0; i < 5; i++) {
+        let random_captch = alphabetArray[Math.floor(Math.random() * alphabetArray.length)];
+
+
+        captcha_box.innerText += ` ${random_captch}`;
+        captcha_box.style.fontSize = "30px";
+
+    }
+}
+generate_captcha();
+
+refresh.addEventListener("click", () => {
+    captcha_box.innerText = "";
+
+});
+
+let submit = document.querySelector("#check_captcha");
+
+submit.addEventListener("click", () => {
+    const check = document.querySelector(".preview").value.split('').join(' ');
+    console.log("done")
+    if (check === captcha_box.innerText) {
+        console.log(true)
+    }
+    else if (check === "") {
+        alert("Please Valid Enter captcha !")
+    }
+    else {
+        alert("Entered Captcha is Invalid !");
+        location.reload();
+    }
+})
+
+function show_password() {
+    let pass = document.querySelector("#password");
+    console.log("password area")
+    if (pass.type === "password") {
+        pass.type = "text";
+    }
+    else {
+        pass.type = "password";
+    }
+
+}
